686b268dd42b89050b17fcc134ed75ca830d1550
Jason Beh <behjieshen@gmail.com>
2021-01-27 19:27:10 +0800

    Commented out nextAuth logic

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,57 +1,57 @@
-import NextAuth from "next-auth";
-import Providers from "next-auth/providers";
-import dbConnect from "../../../utils/mongodb";
-import User from "../../../models/User";
+// import NextAuth from "next-auth";
+// import Providers from "next-auth/providers";
+// import dbConnect from "../../../utils/mongodb";
+// import User from "../../../models/User";
 
-const options = {
-  providers: [
-    Providers.Google({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-    }),
-  ],
-  callbacks: {
-    signIn: async (user, account, profile) => {
-      const { name, email, image } = user;
+// const options = {
+//   providers: [
+//     Providers.Google({
+//       clientId: process.env.GOOGLE_ID,
+//       clientSecret: process.env.GOOGLE_SECRET,
+//     }),
+//   ],
+//   callbacks: {
+//     signIn: async (user, account, profile) => {
+//       const { name, email, image } = user;
 
-      try {
-        await dbConnect();
-        let user = await User.findOne({ email });
+//       try {
+//         await dbConnect();
+//         let user = await User.findOne({ email });
 
-        if (!user) {
-          user = new User({
-            name,
-            email,
-            image,
-          });
-          await user.save();
-        }
+//         if (!user) {
+//           user = new User({
+//             name,
+//             email,
+//             image,
+//           });
+//           await user.save();
+//         }
 
-        return Promise.resolve(true);
-      } catch (err) {
-        console.log("Server error");
-        console.log(err);
-        throw new Error("Error in signin callback");
-      }
-    },
+//         return Promise.resolve(true);
+//       } catch (err) {
+//         console.log("Server error");
+//         console.log(err);
+//         throw new Error("Error in signin callback");
+//       }
+//     },
 
-    session: async (session, user) => {
-      try {
-        await dbConnect();
-        let dbUser = await User.findOne({ email: session.user.email });
-        session.dbUser = dbUser;
-        session.id = dbUser._id;
-      } catch (err) {
-        console.log("Server error");
-        console.log(err);
-        throw new Error("Error in session callback");
-      }
-      return Promise.resolve(session);
-    },
-  },
+//     session: async (session, user) => {
+//       try {
+//         await dbConnect();
+//         let dbUser = await User.findOne({ email: session.user.email });
+//         session.dbUser = dbUser;
+//         session.id = dbUser._id;
+//       } catch (err) {
+//         console.log("Server error");
+//         console.log(err);
+//         throw new Error("Error in session callback");
+//       }
+//       return Promise.resolve(session);
+//     },
+//   },
 
-  // Enable debug messages in the console if you are having problems
-  debug: true,
-};
+//   // Enable debug messages in the console if you are having problems
+//   debug: true,
+// };
 
-export default (req, res) => NextAuth(req, res, options);
+// export default (req, res) => NextAuth(req, res, options);
